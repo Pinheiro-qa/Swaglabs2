44b9a49d94bf0be8abb18909b3412dc0bf67e592
José Pinheiro Chaves Neto <pinheiro.qa@gmail.com>
2022-09-30 10:05:38 -0300

    comp

--- src/test/java/com/swaglabs/tests/pages/IncluirNoCarrinho.java
+++ src/test/java/com/swaglabs/tests/pages/IncluirNoCarrinho.java
@@ -9,21 +9,83 @@ import org.openqa.selenium.support.FindBy;
 public class IncluirNoCarrinho extends BasePage {
 
     @FindBy(id = "add-to-cart-sauce-labs-backpack")
-    private WebElement item;
+    private WebElement item1;
+    @FindBy(id ="add-to-cart-sauce-labs-bike-light")
+    private WebElement item2;
+    @FindBy(id = "add-to-cart-sauce-labs-bolt-t-shirt")
+    private WebElement item3;
+    @FindBy(id = "add-to-cart-sauce-labs-fleece-jacket")
+    private WebElement item4;
+    @FindBy(id = "add-to-cart-sauce-labs-onesie")
+    private WebElement item5;
+    @FindBy(id = "add-to-cart-test.allthethings()-t-shirt-(red)")
+    private WebElement item6;
+    @FindBy(id = "remove-sauce-labs-bike-light")
+    private WebElement removeBotton;
+    @FindBy(className = "title")
+    private WebElement titulo;
     @FindBy(id = "shopping_cart_container")
     private WebElement carrinho;
     @FindBy(className = "cart_quantity")
     private WebElement qtde;
+    @FindBy(id = "checkout")
+    private WebElement checkout;
+    @FindBy(id = "first-name")
+    private WebElement nome;
+    @FindBy(id = "last-name")
+    private WebElement sobrenome;
+    @FindBy(id = "postal-code")
+    private WebElement cep;
+    @FindBy(id = "continue")
+    private WebElement avancar;
+
 
     public IncluirNoCarrinho(WebDriver navegador) {
         super(navegador);
     }
     public EntrarNaPagina incluirProdutoNoCarrinho(){
-        item.click();
+        item1.click();
         carrinho.click();
         return new EntrarNaPagina(navegador);
     }
     public String incluirItemNoCarrinho(){
         return qtde.getText();
+    }
+    public EntrarNaPagina excluirProdutoDoCarrinho(){
+        item2.click();
+        carrinho.click();
+        removeBotton.click();
+        return new EntrarNaPagina(navegador);
+    }
+    public String verificarCarrinho(){
+        return titulo.getText();
+    }
+    public EntrarNaPagina fazerCheckoutNoCarrinho(){
+        item3.click();
+        item4.click();
+        item5.click();
+        item6.click();
+        carrinho.click();
+        checkout.click();
+        return new EntrarNaPagina(navegador);
+    }
+    public String verificarCheckout(){
+        return titulo.getText();
+    }
+    public EntrarNaPagina preencherDadosValidosCheckout(){
+        item3.click();
+        item4.click();
+        item5.click();
+        item6.click();
+        carrinho.click();
+        checkout.click();
+        nome.sendKeys("Joe");
+        sobrenome.sendKeys("Pinetree");
+        cep.sendKeys("60175047");
+        avancar.click();
+        return new EntrarNaPagina(navegador);
+    }
+    public String verificarCheckoutComplete(){
+        return titulo.getText();
     }
 }
--- src/test/java/com/swaglabs/tests/teste/SwagLabs.java
+++ src/test/java/com/swaglabs/tests/teste/SwagLabs.java
@@ -7,17 +7,21 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import javax.swing.plaf.basic.BasicButtonUI;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 @DisplayName("Testes Automatizado do SwagLabs")
 public class SwagLabs {
-    private WebDriver navegador;
+    private WebDriver navegador; // Atributo privado
     @BeforeEach
     public void setUp(){
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
         WebDriverManager.chromedriver().setup();
-        navegador = new ChromeDriver();
+        navegador = new ChromeDriver(options);
         navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
     }
     @Test
@@ -42,6 +46,46 @@ public class SwagLabs {
                 .incluirItemNoCarrinho();
         Assertions.assertEquals("1", itemNoCart);
     }
+    @Test
+    @DisplayName("Excluir Produtos do Carrinho")
+    public void excluirProdutoDoCarrinho(){
+        new EntrarNaPagina(navegador)
+                .acessarPaginaDoSwagLabs()
+                .fazerLoginNaPagina()
+                .entrouNaPagina();
+        new IncluirNoCarrinho(navegador)
+                .excluirProdutoDoCarrinho();
+        String newCart = new IncluirNoCarrinho(navegador)
+                .verificarCarrinho();
+        Assertions.assertEquals("YOUR CART",newCart );
+    }
+    @Test
+    @DisplayName("Fazer Checkout do Carrinho")
+    public void fazerCheckoutDoCarrinho(){
+        new EntrarNaPagina(navegador)
+                .acessarPaginaDoSwagLabs()
+                .fazerLoginNaPagina()
+                .entrouNaPagina();
+        new IncluirNoCarrinho(navegador)
+                .fazerCheckoutNoCarrinho();
+        String checkout = new IncluirNoCarrinho(navegador)
+                .verificarCheckout();
+        Assertions.assertEquals("CHECKOUT: YOUR INFORMATION", checkout);
+    }
+    @Test
+    @DisplayName("Checkout com Dados Validos")
+    public void preencherDadosValidosCheckout() {
+        new EntrarNaPagina(navegador)
+                .acessarPaginaDoSwagLabs()
+                .fazerLoginNaPagina()
+                .entrouNaPagina();
+        new IncluirNoCarrinho(navegador)
+                .preencherDadosValidosCheckout();
+        String CheckDadosValidos = new IncluirNoCarrinho(navegador)
+                .verificarCheckoutComplete();
+        Assertions.assertEquals("CHECKOUT: OVERVIEW", CheckDadosValidos);
+    }
+
     @AfterEach
     public void tearDown(){
         navegador.quit();
